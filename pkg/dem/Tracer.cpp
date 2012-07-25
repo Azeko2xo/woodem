3e1359b77fede615adfbfda5e01dd39c9ec820fd
Václav Šmilauer <eu@doxos.eu>
2012-07-25 16:15:27 -0400

    1. Fix colors of compressed traces (now stable at the end)

--- pkg/dem/Tracer.cpp
+++ pkg/dem/Tracer.cpp
@@ -33,17 +33,19 @@ void TraceGlRep::render(const shared_ptr<Node>& n, GLViewInfo*){
 	glDisable(GL_LINE_SMOOTH);
 	glBegin(GL_LINE_STRIP);
 		for(size_t i=0; i<pts.size(); i++){
-			size_t ix;
+			size_t ix; Real relColor;
 			// compressed start from the very beginning, till they reach the write position
 			if(flags&FLAG_COMPRESS){
 				ix=i;
+				relColor=i*1./writeIx;
 				if(ix>=writeIx) break;
 			// cycle through the array
 			} else {
 				ix=(writeIx+i)%pts.size();
+				relColor=i*1./pts.size();
 			}
 			if(!isnan(pts[ix][0])){
-				glColor3v(Tracer::lineColor->color(i*1./pts.size()));
+				glColor3v(Tracer::lineColor->color(relColor));
 				glVertex3v(pts[ix]);
 			}
 		}
@@ -64,22 +66,26 @@ void Tracer::run(){
 	for(const auto& p: *dem.particles){
 		size_t i=p->id;
 		// delete from all
-		if(reset){ for(const auto& n: p->shape->nodes) n->rep.reset(); }
-		if(modulo[0]>0 && (i+modulo[1])%modulo[0]!=0) continue;
-		if(rRange.maxCoeff()>0
+		bool doReset=reset;
+		if(!doReset && modulo[0]>0 && (i+modulo[1])%modulo[0]!=0) doReset=true;
+		if(!doReset && rRange.maxCoeff()>0
 			&& (!dynamic_pointer_cast<Sphere>(p->shape)
 				|| (rRange[0]>0 && p->shape->cast<Sphere>().radius<rRange[0])
 				|| (rRange[1]>0 && p->shape->cast<Sphere>().radius>rRange[1]))
-		) continue;
-		for(const auto& n: p->shape->nodes){
-			if(!n->rep || !dynamic_pointer_cast<TraceGlRep>(n->rep)){
-				n->rep=make_shared<TraceGlRep>();
-				n->rep->cast<TraceGlRep>().pts.resize(num,Vector3r(NaN,NaN,NaN));
-				if(compress>0) n->rep->cast<TraceGlRep>().flags|=TraceGlRep::FLAG_COMPRESS;
-				if(minDist>0) n->rep->cast<TraceGlRep>().flags|=TraceGlRep::FLAG_MINDIST;
+		) doReset=true;
+		if(doReset){
+			for(const auto& n: p->shape->nodes) n->rep.reset();
+		} else {
+			for(const auto& n: p->shape->nodes){
+				if(!n->rep || !dynamic_pointer_cast<TraceGlRep>(n->rep)){
+					n->rep=make_shared<TraceGlRep>();
+					n->rep->cast<TraceGlRep>().pts.resize(num,Vector3r(NaN,NaN,NaN));
+					if(compress>0) n->rep->cast<TraceGlRep>().flags|=TraceGlRep::FLAG_COMPRESS;
+					if(minDist>0) n->rep->cast<TraceGlRep>().flags|=TraceGlRep::FLAG_MINDIST;
+				}
+				TraceGlRep& tr(n->rep->cast<TraceGlRep>());
+				tr.addPoint(n->pos);
 			}
-			TraceGlRep& tr(n->rep->cast<TraceGlRep>());
-			tr.addPoint(n->pos);
 		}
 	}
 	reset=false;
