a785ea8f34a0beb044a19385b01b80ad2fec71eb
Václav Šmilauer <eu@doxos.eu>
2013-03-19 15:32:16 +0100

    1. Add mutexes for extended entries in grid

--- pkg/dem/Grid.cpp
+++ pkg/dem/Grid.cpp
@@ -1,20 +1,53 @@
 // © 2013 Václav Šmilauer <eu@doxos.eu>
 
 #include<woo/pkg/dem/Grid.hpp>
+#include<boost/function_output_iterator.hpp>
+#include<boost/range/algorithm/set_algorithm.hpp>
 
 GridStore::GridStore(const Vector3i& ijk, int l, bool locking){
 	assert(ijk[0]>0); assert(ijk[1]>0); assert(ijk[2]>0); assert(l>0);
-	grid=gridT(boost::extents[ijk[0]][ijk[1]][ijk[2]][l+1]);
+	grid=gridT(boost::extents[ijk[0]][ijk[1]][ijk[2]][l+1],boost::c_storage_order());
 	// check that those are default-initialized (zeros)
 	assert(grid[0][0][0][0]==0);
 	assert(grid[0][0][0][1]==0);
+	gridExx.resize(mutexExMod);
 	if(locking){
 		size_t n=ijk[0]*ijk[1]*ijk[2];
 		mutexes.reserve(n);
 		// mutexes will be deleted automatically
-		for(size_t i=0; i<n; i++) mutexes.push_back(new boost::mutex);
-		assert(mutexes.size()==n);
+		for(size_t i=0; i<n+mutexExMod; i++) mutexes.push_back(new boost::mutex);
+
+		assert(mutexes.size()==n+mutexExMod);
 	}
+}
+
+Vector3i GridStore::lin2ijk(size_t n) const{
+	const auto& shape=grid.shape();
+	return Vector3i(n/(shape[1]*shape[2]),(n%(shape[1]*shape[2]))/shape[2],(n%(shape[1]*shape[2]))%shape[2]);
+}
+
+size_t GridStore::ijk2lin(const Vector3i& ijk) const{
+	const auto& shape=grid.shape();
+	return ijk[0]*shape[1]*shape[2]+ijk[1]*shape[2]+ijk[2];
+}
+
+
+void GridStore::makeCompatible(shared_ptr<GridStore>& g, int l, bool locking) const {
+	auto shape=grid.shape();
+	l=l>0?l:shape[3]; // set to the real desired value of l
+	assert(l>0);
+	auto shape2=g->grid.shape();
+	// create new grid if none or must be resized
+	// (multi_array preserves elements when resizing, which is a copy operation we don't need at all;
+	//  just create a new array from scratch in that case)
+	if(!g || shape[0]!=shape2[0] || shape[1]!=shape2[1] || shape[2]!=shape2[2] || shape2[3]!=l || g->mutexes.size()!=(locking?shape[0]*shape[1]*shape[2]+mutexExMod:0)){
+		g=make_shared<GridStore>(Vector3i(shape[0],shape[1],shape[2]),l>0?l:shape[3],locking=locking);
+		return;
+	}
+	assert(shape[0]==shape2[0] && shape[1]==shape2[1] && shape[2]==shape2[2] && shape2[3]==l);
+	assert((!locking && g->mutexes.size()==0) || (locking && g->mutexes.size()==shape[0]*shape[1]*shape[2]+mutexExMod));
+	assert(gridExx.size()==muteExMod);
+	for(auto gridEx: g->gridExx) gridEx.clear();
 }
 
 void GridStore::checkIndices(const Vector3i& ijk) const {
@@ -23,18 +56,42 @@ void GridStore::checkIndices(const Vector3i& ijk) const {
 	assert(ijk[2]>=0); assert(ijk[2]<grid.shape()[2]);
 }
 
-boost::mutex* GridStore::getMutex(const Vector3i& ijk){
+boost::mutex* GridStore::getMutex(const Vector3i& ijk, bool mutexEx){
 	checkIndices(ijk);
-	const int& i(ijk[0]), &j(ijk[1]), &k(ijk[2]);
-	auto shape=grid.shape();
-	assert(i*shape[1]*shape[2]+j*shape[2]+k<mutexes.size());
-	assert(shape[0]*shape[1]*shape[2]==mutexes.size());
-	return &mutexes[i*shape[1]*shape[2]+j*shape[2]+k];
+	size_t ix=ijk2lin(ijk);
+	if(!mutexEx){
+		assert(ix<mutexes.size()-mutexExMod);
+		assert(shape[0]*shape[1]*shape[2]+mutexExMod==mutexes.size());
+		return &mutexes[ix];
+	} else {
+		assert(mutexExMod>0);
+		return &mutexes[mutexes.size()-(ix%mutexExMod)];
+	}
 }
 
-void GridStore::append(const Vector3i& ijk, const GridStore::id_t& id){
+const GridStore::gridExT& GridStore::getGridEx(const Vector3i& ijk) const {
+	size_t ix=ijk2lin(ijk);
+	assert(mutexExMod>0);
+	assert(gridExx.size()==muteExMod);
+	return gridExx[ix%mutexExMod];
+}
+
+GridStore::gridExT& GridStore::getGridEx(const Vector3i& ijk) {
+	size_t ix=ijk2lin(ijk);
+	assert(mutexExMod>0);
+	assert(gridExx.size()==muteExMod);
+	return gridExx[ix%mutexExMod];
+}
+
+
+void GridStore::protected_append(const Vector3i& ijk, const GridStore::id_t& id){
 	checkIndices(ijk);
 	boost::mutex::scoped_lock lock(*getMutex(ijk));
+	append(ijk,id,/*lockEx*/true);
+}
+
+void GridStore::append(const Vector3i& ijk, const GridStore::id_t& id, bool lockEx){
+	checkIndices(ijk);
 	const int& i(ijk[0]), &j(ijk[1]), &k(ijk[2]);
 	// 0th element is the number of elements
 	int& cellSz=grid[i][j][k][0];
@@ -45,14 +102,21 @@ void GridStore::append(const Vector3i& ijk, const GridStore::id_t& id){
 		grid[i][j][k][cellSz+1]=id;
 	} else {
 		assert(mapInitSz>0);
-		assert((cellSz==denseSz && gridEx.find(ijk)==gridEx.end()) || (cellSz>denseSz && gridEx.find(ijk)!=gridEx.end()));
+		auto& gridEx=getGridEx(ijk);
 		if(cellSz==denseSz){ 
 			// new extension vector; gridEx[ijk] default-constructs vector<id_t>
-			vector<id_t>& ex=gridEx[ijk]; ex.resize(mapInitSz);
+			boost::mutex::scoped_lock lock(*getMutex(ijk,/*mutexEx*/true),boost::defer_lock);
+			if(lockEx) lock.lock();
+			assert(gridEx.find(ijk)==gridEx.end());
+			vector<id_t>& ex=gridEx[ijk];
+			if(lockEx) lock.unlock();
+			ex.resize(mapInitSz);
 			ex[0]=id;
 		} else {
 			// existing extension vector
-			vector<id_t>& ex=gridEx[ijk];
+			auto exI=gridEx.find(ijk);
+			assert(exI!=gridEx.end());
+			vector<id_t>& ex(exI->second);
 			size_t exIx=cellSz-denseSz;
 			assert(exIx>0);
 			assert(exIx<=ex.size());
@@ -63,7 +127,12 @@ void GridStore::append(const Vector3i& ijk, const GridStore::id_t& id){
 	cellSz++;
 }
 
-GridStore::id_t GridStore::get(const Vector3i& ijk, int l) {
+void GridStore::clear_dense(const Vector3i& ijk){
+	checkIndices(ijk);
+	grid[ijk[0]][ijk[1]][ijk[2]][0]=0;
+}
+
+GridStore::id_t GridStore::get(const Vector3i& ijk, int l) const {
 	checkIndices(ijk);
 	assert(l>=0);
 	const int& i(ijk[0]), &j(ijk[1]), &k(ijk[2]);
@@ -71,8 +140,11 @@ GridStore::id_t GridStore::get(const Vector3i& ijk, int l) {
 	__attribute__((unused)) const int cellSz=grid[i][j][k][0];
 	assert(l<cellSz);
 	if(l<denseSz) return grid[i][j][k][l+1];
+	auto& gridEx=getGridEx(ijk);
 	assert(gridEx.find(ijk)!=gridEx.end());
-	const vector<id_t>& ex=gridEx[ijk];
+	auto ijkI=gridEx.find(ijk);
+	assert(ijkI!=gridEx.end());
+	const vector<id_t>& ex=ijkI->second;
 	size_t exIx=l-denseSz;
 	assert(exIx<ex.size() && exIx>=0);
 	return ex[exIx];
@@ -84,11 +156,38 @@ size_t GridStore::size(const Vector3i& ijk) const {
 	return cellSz;
 }
 
-shared_ptr<GridStore> GridStore::relativeComplement(const shared_ptr<GridStore>& g2) const{
-	throw std::runtime_error("GridStore::relativeComplement: not yet implemented.");
+void GridStore::computeRelativeComplements(GridStore& B, shared_ptr<GridStore>& A_B, shared_ptr<GridStore>& B_A){
+	//throw std::runtime_error("GridStore::computeRelativeComplements: not yet implemented.");
+	GridStore& A(*this);
 	auto shape=grid.shape();
-	// return non-locking GridStore
-	shared_ptr<GridStore> ret=make_shared<GridStore>(Vector3i(shape[0],shape[1],shape[2]),shape[3]-1,/*allocMutexes*/false);
-	return ret;
+	assert(A.grid.shape()[0]==B.grid.shape()[0] && A.grid.shape()[1]==B.grid.shape()[1] && A.grid.shape()[2]==B.grid.shape()[2]);
+	// use the same value of L for now
+	makeCompatible(A_B,/*locking*/false); makeCompatible(B_A,/*locking*/false);
+	// traverse all cells in parallel
+	// http://stackoverflow.com/questions/5572464/how-to-traverse-a-boostmulti-array - ??
+	// do it the old way :|
+	size_t N=shape[0]*shape[1]*shape[2];
+	#ifdef WOO_OPENMP
+		#pragma omp parallel for schedule(guided)	
+	#endif
+	for(size_t n=0; n<N; n++){
+		// static_assert(std::is_same<gridT::storage_order_type,boost::c_storage_order>::value,"Storage order of boost::multi_array is not row-major?! Code should be adjusted, as memory access pattern might be iniefficient!");
+		// TODO: assert c storage order here
+		// checked against http://stackoverflow.com/a/12113479/761090
+		Vector3i ijk=lin2ijk(n);
+		checkIndices(ijk);
+		A_B->clear_dense(ijk); B_A->clear_dense(ijk);
+		// uset std::set and set_difference
+		// for very small sets, just two nested loops might be faster
+		#if 1
+			std::set<id_t> setA; size_t maxA=A.size(ijk); for(size_t a=0; a<maxA; a++) setA.insert(A.get(ijk,a));
+			std::set<id_t> setB; size_t maxB=B.size(ijk); for(size_t b=0; b<maxB; b++) setB.insert(B.get(ijk,b));
+			// push to the grid directly from within the algo,
+			// rather than storing the difference in a teporary and copying
+			// http://stackoverflow.com/a/8018943/761090
+			boost::range::set_difference(setA,setB,boost::make_function_output_iterator([&](id_t id){ A_B->append(ijk,id); }));
+			boost::range::set_difference(setB,setA,boost::make_function_output_iterator([&](id_t id){ B_A->append(ijk,id); }));
+		#endif
+	}
 }
 
--- pkg/dem/Grid.hpp
+++ pkg/dem/Grid.hpp
@@ -12,18 +12,37 @@
 	python inspection
 	rudimentary self-tests
 	boost::serialization of multi_array perhaps (storage is guaranteed to be contiguous)?
+
+	3d grid for storing ids. Each cell stores a fixed number of ids in dense static array, and may store additional ids as necessary in several maps, mapping ijk (3d integral coordinate) to vector<id_t>. Those maps are split using modulo of the linear index so that access to those is more fin-egrained than having to lock the whole structure.
+
+	The static (dense) array stores number of items in that cell as the first element.
+
+	Each cell might be protected by a mutex for writing, as well as each map ijk->ids (protected_append).
+
 */
 struct GridStore: public Object{
 	typedef Particle::id_t id_t;
 	enum {ID_NONE=-1 };
-	struct Vector3iComparator{ bool operator()(const Vector3i& a, const Vector3i& b){ return (a[0]<b[0] || (a[0]==b[0] && (a[1]<b[1] || (a[1]==b[1] && a[2]<b[2])))); };	};
+	struct Vector3iComparator{ bool operator()(const Vector3i& a, const Vector3i& b) const { return (a[0]<b[0] || (a[0]==b[0] && (a[1]<b[1] || (a[1]==b[1] && a[2]<b[2])))); };	};
 	typedef boost::multi_array<id_t,4> gridT;
 	typedef std::map<Vector3i,vector<id_t>,Vector3iComparator> gridExT;
 	typedef boost::ptr_vector<boost::mutex> mutexesT;
 	gridT grid;
-	gridExT gridEx;
-	mutexesT mutexes;
+	// initial size of vector in the map
 	const int mapInitSz=3;
+	// use several maps so that write-access can be mutually exclusive in a finer way
+	const int mutexExMod=10;
+	vector<gridExT> gridExx;
+	mutexesT mutexes;
+
+	// obtain gridEx for given cell
+	const gridExT& getGridEx(const Vector3i& ijk) const;
+	// non-const reference, otherwise the same
+	gridExT& getGridEx(const Vector3i& ijk);
+
+	Vector3i lin2ijk(size_t n) const;
+	size_t ijk2lin(const Vector3i& ijk) const;
+
 	/*
 		the first ID is not actually ID, but the number of IDs for that cell;
 		it makes iteration faster, especially there is no lookup into the map
@@ -32,21 +51,34 @@ struct GridStore: public Object{
 
 	// ctor; allocate grid and locks (if desired)
 	GridStore(const Vector3i& ijk, int l, bool locking=true);
+
+	// set g to be same-shape grid witout mutexes and gridEx 
+	// if l is given and positive, use that value instead of the current shape[3]
+	// grid may contain garbage data!
+	void makeCompatible(shared_ptr<GridStore>& g, int l=0, bool locking=true) const;
+
 	// return lock pointer for given cell
-	boost::mutex* getMutex(const Vector3i& ijk);
+	boost::mutex* getMutex(const Vector3i& ijk, bool mutexEx=false);
 	// assert that indices are OK, no-op in optimizes build
 	void checkIndices(const Vector3i& ijk) const;
 
-	// lock and add element to the cell (in grid, or, if full, in gridEx)
-	void append(const Vector3i& ijk, const id_t&);
+	// thread unsafe: clear dense storage
+	void clear_dense(const Vector3i& ijk);
+	// thread unsafe: add element to the cell (in grid, or, if full, in gridEx)
+	void append(const Vector3i& ijk, const id_t&, bool lockEx=false);
+	// thread safe: lock and append
+	void protected_append(const Vector3i& ijk, const id_t&);
 
-	// compute relative complement of this WRT g2, and return it
-	// (i.e. return grid with ids in g2 but NOT in this)
+	// compute relative complements (this\B) and (B\this)
+	// (i.e. return grid with ids in g2 but NOT in this and vice versa)
 	// this function is internally parallelized, and needs no locking
-	shared_ptr<GridStore> relativeComplement(const shared_ptr<GridStore>& g2) const;
+	//
+	// A_B contains only elements in B but not in A
+	// B_A contains only elements in A but not in B
+	void computeRelativeComplements(GridStore& B, shared_ptr<GridStore>& A_B, shared_ptr<GridStore>& B_A);
 
 	// return i-th element, from grid or gridEx depending on l
-	id_t get(const Vector3i& ijk, int l);
+	id_t get(const Vector3i& ijk, int l) const;
 	// return number of elements in grid+gridEx
 	size_t size(const Vector3i& ijk) const;
 };
