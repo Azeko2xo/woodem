8e5300cfa056330c69a481bc9cb6d93c6720f07a
Václav Šmilauer <eu@doxos.eu>
2014-02-01 21:05:31 +0100

    Another fux for multiple defiunition

--- core/Dispatcher.cpp
+++ core/Dispatcher.cpp
@@ -1,17 +1,5 @@
-/*************************************************************************
-*  Copyright (C) 2004 by Olivier Galizzi                                 *
-*  olivier.galizzi@imag.fr                                               *
-*                                                                        *
-*  This program is free software; it is licensed under the terms of the  *
-*  GNU General Public License v2 or later. See file LICENSE for details. *
-*************************************************************************/
-
-
-#include<woo/core/Functor.hpp>
+#include<woo/core/Master.hpp>
 #include<woo/core/Dispatcher.hpp>
-#include<algorithm>
-#include<vector>
-
 WOO_PLUGIN(core,(Dispatcher));
 
 
