f5b9dc363834420de348d015cd9fa60981bb77d5
Václav Šmilauer <eu@doxos.eu>
2014-09-02 13:26:21 +0200

    1. fix tab/space

--- py/ipythonintegration.py
+++ py/ipythonintegration.py
@@ -145,7 +145,7 @@ def create_inputhook_qt4(mgr, app=None):
 
 def replaceInputHookIfNeeded():
     import woo.runtime, woo.config
-	 if 'qt4' not in woo.config.features: return
+    if 'qt4' not in woo.config.features: return
     ## apply local version instead of the stock one
     if woo.runtime.ipython_version() in (12,13,100,110,120):
         import IPython.lib.inputhookqt4
